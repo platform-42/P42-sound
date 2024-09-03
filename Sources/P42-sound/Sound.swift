e982bfec06e9c710d5561ce0d34ab52c1faccd3a
Diederick de Buck <diederick.de.buck@platform-42.com>
2024-09-03 15:17:19 +0200

    ddddd

--- Sources/P42-sound/Sound.swift
+++ Sources/P42-sound/Sound.swift
@@ -1,2 +1,52 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+//
+//  Sound.swift
+//  shops24
+//
+//  Created by Diederick de Buck on 22/11/2022.
+//
+
+import Foundation
+import AVFoundation
+
+
+public enum SoundId: SystemSoundID {
+    case click = 1306
+    case lock = 1100
+}
+
+
+public enum SoundFile: String {
+    case trash
+    case reject
+}
+
+
+public enum SoundExtension: String {
+    case mp3
+    case wav
+    case aif
+}
+
+
+public class Sound {
+    
+    public static func playSound(_ soundId: SoundId, audible: Bool) {
+        if (audible) {
+            AudioServicesPlaySystemSound(soundId.rawValue)
+        }
+    }
+    
+    public static func playSound(_ soundFile: SoundFile, soundExtension: SoundExtension, audible: Bool) {
+        if (audible) {
+            let filePath: String? = Bundle.main.path(
+                forResource: soundFile.rawValue,
+                ofType: soundExtension.rawValue
+            )!
+            let fileURL: NSURL = NSURL(fileURLWithPath: filePath!)
+            var soundID: SystemSoundID = 0
+            AudioServicesCreateSystemSoundID(fileURL, &soundID)
+            AudioServicesPlaySystemSound(soundID)
+        }
+    }
+}
+
