13d602369020a9030089990377ec17cf27711c22
Diederick de Buck <diederick.de.buck@platform-42.com>
2025-03-14 12:00:08 +0100

    more sounds

--- Sources/P42-sound/Sound.swift
+++ Sources/P42-sound/Sound.swift
@@ -13,9 +13,8 @@ public enum SoundId: SystemSoundID {
     case click = 1306
     case lock = 1100
     case reject = 1053
-    case tock = 1024
-    case chime = 1029
-    case error = 1011
+    case tock = 1008
+    case alert = 1005
 }
 
 
